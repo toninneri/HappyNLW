c3a70586e6c233d17e4b22acedc189af98e8759c
toninneri <toninneri@gmail.com>
2020-10-17 19:10:18 -0300

    App Done

---
+++ web/src/routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanagesMaps from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+        <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app"  component={OrphanagesMaps} />
+
+        <Route path="/orphanages/create"  component={CreateOrphanage} />
+        <Route path="/orphanages/:id"  component={Orphanage} />
+        </Switch>
+        </BrowserRouter>
+
+    );
+}
+
+export default Routes;
